f7be86c4167dc86661acfd4766d5bee68666a63a
Sveta <lana.yurkevich@icloud.com>
2024-03-19 14:52:24 +0000

    Add save and read UserDefaults

--- Mock Interview/ViewModel.swift
+++ Mock Interview/ViewModel.swift
@@ -29,4 +29,25 @@ class ViewModel {
             state = .feedback
         }
     }
+    
+    /// Save 3 responses
+    func saveQuestions(questions: [String]) {
+        UserDefaults.standard.setValue(questions[0], forKey: "question-1")
+        UserDefaults.standard.setValue(questions[1], forKey: "question-2")
+        UserDefaults.standard.setValue(questions[2], forKey: "question-3")
+    }
+    
+    // Get 3 saved quesitons from user defaults.
+    func readQuestions() -> [String] {
+        guard let question1 = UserDefaults.value(forKey: "question-1") as? String else {
+            return []
+        }
+        guard let question2 = UserDefaults.value(forKey: "question-2") as? String else {
+            return []
+        }
+        guard let question3 = UserDefaults.value(forKey: "question-3") as? String else {
+            return []
+        }
+        return [question1, question2, question3]
+    }
 }
